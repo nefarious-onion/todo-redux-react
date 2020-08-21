d390502ad80ce222bfbcd56053615d51c3ca4f71
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-21 15:19:41 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
+
 
 const App = () => {
     return (
@@ -13,6 +17,9 @@ const App = () => {
                <Switch>
                    <Route path='/' exact component={Dashboard} />
                    <Route path='/project/:id' component={ProjectDetails}/>
+                   <Route path='/signin' component={SignIn}/>
+                   <Route path='/signup' component={SignUp}/>
+                   <Route path='/create' component={CreateProject}/>
                </Switch>
             </div>
         </BrowserRouter>
