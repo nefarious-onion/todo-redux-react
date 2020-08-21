d0d47277b902bdfbca7feb31882e8677ad0ee9a2
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-21 15:21:38 +0300

    update navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ import SignedOutLinks from './SignedOutLinks';
 const Navbar = () => {
     return (
         <div>
-            <nav className="nav-wrapper grey darken-3">
+            <nav className="nav-wrapper darken-3">
                 <div className="container">
                     <Link to='/' className='brand-logo'>react redux todo</Link>
                     <SignedOutLinks/>
