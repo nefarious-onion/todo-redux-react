8a9a3a0df373f49b0482ce5d6596112d8a14667b
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-23 14:58:19 +0300

    add reducers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//components
+import App from './App';
+//reducers
+import rootReducer from './store/reducers/rootReducer';
+//styles
 import './index.css';
 
-import App from './App';
+const store = createStore(rootReducer);
 
-    
-    ReactDOM.render(
-    
-      <App />
-    ,
-    document.getElementById("root")
-  );
-  serviceWorker.register();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById("root")
+);
+serviceWorker.register();
