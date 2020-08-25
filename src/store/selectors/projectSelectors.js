9d668e1d552616ee86c8fa21e36b027484a20c5f
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 20:51:25 +0300

    add getProject selector

--- src/store/selectors/projectSelectors.js
+++ src/store/selectors/projectSelectors.js
@@ -1,1 +1,6 @@
 export const getProjects = state => state.firestore.ordered.projects;
+
+export const getProject = id => state => {
+    const projects = state.firestore.data.projects;
+    return projects ? projects[id] : null;
+};
