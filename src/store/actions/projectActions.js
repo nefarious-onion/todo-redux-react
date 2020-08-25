4885bae22f7c5348d3baf57e7e2ebfdfcbf51f44
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 17:09:31 +0300

    make it pretty

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,19 +1,22 @@
 export const createProject = ({ firestore }, project) => {
     console.log(firestore);
-    
+
     return (dispatch, getState) => {
         //make async call to db
-        firestore.collection('projects').add({
-            ...project,
-            authorFirstName: 'Onion',
-            authorLastName: 'Nefarious',
-            authorId: 12345,
-            createdAt: new Date()
-        }).then(() => {
-            dispatch({ type: 'CREATE_PROJECT', project })
-
-        }).catch(error => {
-            dispatch({ type: 'CREATE_PROJECT_ERROR', error })
-        })
+        firestore
+            .collection('projects')
+            .add({
+                ...project,
+                authorFirstName: 'Onion',
+                authorLastName: 'Nefarious',
+                authorId: 12345,
+                createdAt: new Date()
+            })
+            .then(() => {
+                dispatch({ type: 'CREATE_PROJECT', project })
+            })
+            .catch(error => {
+                dispatch({ type: 'CREATE_PROJECT_ERROR', error })
+            })
     }
 }
