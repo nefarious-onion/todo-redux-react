199d33b91f25a2410afa9b8ae1b2d981491cbf5f
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 16:56:21 +0300

    add error handling

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,11 +9,14 @@ const initState = {
 const projectReducer = (state = initState, action) => {
     switch (action.type) {
         case 'CREATE_PROJECT':
-                console.log('project created', action.project)
-
-            
+                console.log('project created', action.project);
+                return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.error);
+            return state
+        default:
+            return state
     }
-    return state
 }
 
 export default projectReducer;
