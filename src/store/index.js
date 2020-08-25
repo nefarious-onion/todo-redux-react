303a0183e62197aec47263b3ccb366ca3b53b8b7
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 16:58:37 +0300

    refactor store to its own file, add enhancers

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+//reducers
+import rootReducer from "./reducers/rootReducer";
+
+const composeEnhancers = composeWithDevTools({
+	realtime: true
+	// options like actionSanitizer, stateSanitizer
+});
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(
+        //applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+        applyMiddleware(thunk)
+    )
+);
+
+export default store;
