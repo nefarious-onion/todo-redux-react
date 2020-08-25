8a80596a693836b202e1133e72fe42655da2c7f3
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 20:51:08 +0300

    add dynamic content

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 //components
 import ProjectSummary from './ProjectSummary';
 
 const ProjectList = ({ projects }) => {
 
     const renderedList = projects && projects.map(project => {
-        return <ProjectSummary  key={project.id} project={project}/>
+        return (
+            <Link to={`/project/${project.id}`} key={project.id}>
+                <ProjectSummary  project={project} />
+            </Link>
+        )
     })
     return (
         <div className='project-list section'>
