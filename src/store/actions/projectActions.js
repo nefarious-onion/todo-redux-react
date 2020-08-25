44102b8d021cb6ae746d3ab25c7763cb2fce1451
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 13:32:38 +0300

    add db connection to create project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,14 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to db
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Onion',
+            authorLastName: 'Nefarious',
+            authorId: 12345,
+            createdAt: new Date()
+        })
         dispatch({type: 'CREATE_PROJECT', project})
     }
 }
