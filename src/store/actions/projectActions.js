34bab4d66f78afc48bd458eaf4546c7cd0306a2a
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-25 16:57:52 +0300

    fix firestore code, add error handling, finalize create project action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,14 +1,19 @@
-export const createProject = (project) => {
-    return (dispatch, getState, { getFirebase, getFirestore }) => {
+export const createProject = ({ firestore }, project) => {
+    console.log(firestore);
+    
+    return (dispatch, getState) => {
         //make async call to db
-        const firestore = getFirestore();
         firestore.collection('projects').add({
             ...project,
             authorFirstName: 'Onion',
             authorLastName: 'Nefarious',
             authorId: 12345,
             createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project })
+
+        }).catch(error => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', error })
         })
-        dispatch({type: 'CREATE_PROJECT', project})
     }
 }
