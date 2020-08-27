25e61316c579e53317ddb7d8b9f253886403e357
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-27 21:43:17 +0300

    add signin logic, add route guard for logged-in-users, add login error message

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { Redirect } from 'react-router-dom';
+//redux
+import { useDispatch, useSelector } from 'react-redux';
+import { useFirebase } from 'react-redux-firebase';
+//actions
+import { signIn } from '../../auth/auth.action';
+//selectors
+import { getAuthError, getAuth } from '../../auth/auth.selector';
 
 const SignIn = () => {
+    const firebase = useFirebase();
+    const dispatch = useDispatch();
+    const newSignIn = useCallback( credentials => 
+        dispatch(signIn(
+            { firebase }, credentials)
+        ), [ dispatch, firebase ]
+    );
+    const authError = useSelector(getAuthError);
+    const auth = useSelector(getAuth);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
     const onSubmit = event => {
         event.preventDefault();
         console.log(email, password);
+        console.log(typeof email);
         
+        const credentials = {
+            email,
+            password
+        }
+        newSignIn(credentials)
     }
-
+    
+    if (auth.uid) return <Redirect to='/' />
     return (
         <div className='container'>
             <form className='white' onSubmit={onSubmit}>
@@ -34,6 +59,12 @@ const SignIn = () => {
                 </div>
                 <div className="input-field">
                     <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    <div className="red-text center">
+                        {authError
+                            ? <p>{authError}</p>
+                            : null
+                        }
+                    </div>
                 </div>
             </form>
             
