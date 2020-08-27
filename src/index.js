3fe1507722aa77cbde8fccc0aae6ab0ef9c7482c
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-27 21:37:53 +0300

    add check for authIsLoaded

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 //redux
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 //firebase
 import { createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { ReactReduxFirebaseProvider, isLoaded } from 'react-redux-firebase';
 import firebase from './firebase/firebase';
 //store
 import store from './store';
 //components
 import App from './App';
+//selectors
+import { getAuth } from './auth/auth.selector';
 //styles
 import './index.css';
 
@@ -18,22 +20,31 @@ const rrfConfig = {
   userProfile: 'users',
   attachAuthIsReady: true,
   useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-  };
+};
 
-  const rrfProps = {
+const rrfProps = {
   firebase,
   config: rrfConfig,
   dispatch: store.dispatch,
   createFirestoreInstance // <- needed if using firestore
-  };
+};
+
+const AuthIsLoaded = ({ children }) => {
+  const auth = useSelector(getAuth);
+  if (!isLoaded(auth)) {
+    return <div>Loading page...</div>
+  }
+  return children;
+}
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
-  </Provider>
-  ,
+  </Provider>,
   document.getElementById("root")
 );
 serviceWorker.register();
