6c98ac90cfe945fc431b3b2d43b9175b90ec7e62
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-27 21:41:51 +0300

    add signout logic

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { NavLink } from 'react-router-dom';
+//redux
+import { useFirebase } from 'react-redux-firebase';
+import { useDispatch } from 'react-redux';
+//actions
+import { signOut } from '../../auth/auth.action';
 
-const SignedInLinks = () => {
+
+const SignedInLinks = ({ profile }) => {
+    const firebase = useFirebase();
+    const dispatch = useDispatch();
+    const logOut = useCallback(() =>
+        dispatch(signOut(
+            { firebase })
+        ), [dispatch, firebase]
+    );
+
     return (
         <ul className='right'>
             <li><NavLink to='/create'>New Project</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
-            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>Onion</NavLink></li>
+            <li><a onClick={logOut} to='/'>Log Out</a></li>
+            <li><NavLink to='/' className='btn btn-floating pink lighten-1'>{profile.initials}</NavLink></li>
         </ul>
     );
 }
