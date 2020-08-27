fede0cf8abb6ce576088de91b6b663e494cb3663
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-27 21:41:03 +0300

    add conditional rendering for links, get profile information from firebase

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+//redux
+import { useSelector } from 'react-redux';
+//selectors
+import { getAuth } from '../../auth/auth.selector';
+import { getProfile } from '../../users/users.selector';
 //components
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
+
 const Navbar = () => {
+    const auth = useSelector(getAuth);
+    const profile = useSelector(getProfile);
+
+    const links = auth.uid
+                    ? <SignedInLinks profile={profile} />
+                    : <SignedOutLinks />;
+
     return (
         <div>
             <nav className="nav-wrapper darken-3">
                 <div className="container">
                     <Link to='/' className='brand-logo'>react redux todo</Link>
-                    <SignedOutLinks/>
-                    <SignedInLinks/>
+                    { links }
                 </div>
             </nav>
         </div>
