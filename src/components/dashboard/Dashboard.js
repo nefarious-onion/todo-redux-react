01c2bee150bd218ab0d22d26ae83a247af38e59c
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-27 21:40:13 +0300

    add route guard for non-loggedin-users

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+//redux
 import { useSelector } from 'react-redux';
 import {  useFirestoreConnect } from 'react-redux-firebase';
 //selectors
 import { getProjects } from '../../projects/projects.selector';
+import { getAuth } from '../../auth/auth.selector';
 //components
 import Notifications from '../Notifications/Notifications';
 import ProjectList from '../ProjectList/ProjectList';
@@ -11,6 +14,9 @@ import ProjectList from '../ProjectList/ProjectList';
 const Dashboard = () => {
     useFirestoreConnect('projects');
     const projects = useSelector(getProjects);
+    const auth = useSelector(getAuth);
+
+    if (!auth.uid) return <Redirect to='/signin' />
     
     return (
         <div className='dashboard container'>
