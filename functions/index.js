df4da33daf1835506a548cb61d49737eec310a3d
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-29 15:33:38 +0300

    add cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase)
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello turds");
+});
+
