8de67bdffc14aaa483f6d4fb80f7b9fee2ec9b6c
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-29 15:43:54 +0300

    production version

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-admin.initializeApp(functions.config().firebase)
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello turds");
-});
-
