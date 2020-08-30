c4aac2f66a36b51a305937fd279a8ebf893cb33e
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-30 22:22:24 +0300

    style component

--- src/components/ProjectSummary/ProjectSummary.js
+++ src/components/ProjectSummary/ProjectSummary.js
@@ -6,10 +6,12 @@ const ProjectSummary = ({ project }) => {
 
     return (
 
-        <div className="card z-depth-0 project-summary">
-            <span className="card-title">{project.title}</span>
-            <p>{project.authorFirstName} {project.authorLastName}</p>
-            <p className="grey-text">{date}</p>
+        <div className="card project-summary">
+            <div className="card-content teal-text text-darken-3">
+                <span className="card-title color-hover">{project.title}</span>
+                <p>{project.authorFirstName} {project.authorLastName}</p>
+                <p className="project-date">{date}</p>
+            </div>
         </div>
 
     );
