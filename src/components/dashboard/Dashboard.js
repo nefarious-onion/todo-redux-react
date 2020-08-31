1cacb638d4772588dd951102eda5c9208e38051a
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-31 12:08:12 +0300

    make projects bigger on dashboard, remove notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,7 +7,6 @@ import { useFirestoreConnect } from 'react-redux-firebase';
 import { getProjects } from '../../projects/projects.selector';
 import { getAuth } from '../../auth/auth.selector';
 //components
-import Notifications from '../Notifications/Notifications';
 import ProjectList from '../ProjectList/ProjectList';
 
 
@@ -27,11 +26,8 @@ const Dashboard = () => {
     return (
         <div className='dashboard container'>
             <div className="row">
-                <div className="col s12 m6">
+                <div className="col s12">
                     <ProjectList projects={projects} />
-                </div>
-                <div className="col s12 m5 offset-m1">
-                    <Notifications />
                 </div>
             </div>
         </div>
