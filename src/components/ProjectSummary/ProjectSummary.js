1ea7a0a9e79c689266a3681636a3f7842d4c7250
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-31 12:07:37 +0300

    add button for complete

--- src/components/ProjectSummary/ProjectSummary.js
+++ src/components/ProjectSummary/ProjectSummary.js
@@ -5,15 +5,14 @@ const ProjectSummary = ({ project }) => {
     const date = moment(project.createdAt.toDate()).calendar();
 
     return (
-
         <div className="card project-summary">
             <div className="card-content teal-text text-darken-3">
+            <button className='waves-effect waves-light btn-large green accent-3 right ' id='detailButton' ><i className="material-icons">done</i></button>
                 <span className="card-title color-hover">{project.title}</span>
                 <p>{project.authorFirstName} {project.authorLastName}</p>
                 <p className="project-date">{date}</p>
             </div>
         </div>
-
     );
 }
 
